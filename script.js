2123de7c00093e9ce22f565b521f09728d0b4d83
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-05-12 20:48:26 +0200

    rendrefalse

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const reponse = document.getElementById("reponse");
-console.log(reponse);
+
+const ul1 = document.querySelectorAll(".un li");
+const ul2 = document.querySelectorAll(".deux li");
+
+function tousfalse(sauf) {
+  mettre,
+    (decimettre = false),
+    (centimetre = false),
+    (millimettre = false),
+    (kilomettre = false);
+  sauf = true;
+}
 let mettre,
   decimettre,
   centimetre,
-  millimettre = false;
-
+  millimettre,
+  kilomettre = false;
 input.addEventListener("input", (e) => {
   let valeur = e.target.value;
   console.log(e.target.value);
@@ -19,6 +30,9 @@ input.addEventListener("input", (e) => {
 });
 
 function Kilomettre(valeur) {
+  if (kilomettre == true) {
+    reponse.innerHTML = valeur / 1000 + "m";
+  }
   if (mettre == true) {
     reponse.innerHTML = valeur / 1000 + "m";
   } else if (decimettre == true) {
@@ -29,3 +43,58 @@ function Kilomettre(valeur) {
     reponse.innerHTML = valeur * 10000000 + "mm";
   }
 }
+let functionajouer;
+ul1.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    functionajouer = e.target.id;
+    console.log("function a jouer : " + functionajouer);
+  });
+});
+ul2.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    if (e.target.id == "Kilomettre2") {
+      kilomettre = true;
+      tousfalse(kilomettre);
+      console.log(kilomettre);
+    }
+    if (e.target.id == "mettre2") {
+      tousfalse(mettre);
+      mettre = true;
+      console.log(mettre);
+    }
+    if (e.target.id == "decimetttre2") {
+      tousfalse(decimettre);
+      decimettre = true;
+      console.log(decimettre);
+    }
+    if (e.target.id == "centimettre2") {
+      tousfalse(centimetre);
+      centimetre = true;
+      console.log(centimetre);
+    }
+    if (e.target.id == "milimettre2") {
+      tousfalse(millimettre);
+      millimettre = true;
+      console.log(millimettre);
+    }
+  });
+});
+function execution(functionn) {
+  if (kilomettre == true) {
+    functionn(kilomettre);
+  }
+  if (mettre == true) {
+    functionn(mettre);
+  }
+  if (decimettre == true) {
+    functionn(decimettre);
+  }
+  if (centimetre == true) {
+    functionn(centimetre);
+  }
+  if (millimettre == true) {
+    functionn(millimettre);
+  }
+}
+
+execution(functionajouer);
