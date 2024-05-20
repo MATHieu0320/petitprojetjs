a943436b9f5579a2cdc0a643338f9f482b07cbb0
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-05-20 19:18:55 +0200

    Update index.jsss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { log } from "console";
 import { Kilomettreilomettre } from "./EnQuoiConvertir";
+import { lchown } from "fs/promises";
 
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
@@ -14,16 +15,20 @@ let mettre,
   kilomettre = false;
 let functionajouer;
 
-setInterval(() => {
-  console.log(kilomettre + "kilomettre");
-  console.log(mettre + "  mettre");
-  console.log(decimettre + "  decimettre");
-  console.log(centimetre + "  centometre");
-  console.log(millimettre + "  millimettre");
-}, 10000);
+// setInterval(() => {
+//   console.log(kilomettre + "kilomettre");
+//   console.log(mettre + "  mettre");
+//   console.log(decimettre + "  decimettre");
+//   console.log(centimetre + "  centometre");
+//   console.log(millimettre + "  millimettre");
+// }, 10000);
+
 ul1.forEach((li) => {
   li.addEventListener("click", (e) => {
     functionajouer = e.target.id;
+    document.querySelector(".active")?.classList.remove("active");
+    li.classList.add("active");
+    console.log(functionajouer);
     console.log("function a jouer : " + functionajouer);
   });
 });
@@ -32,6 +37,8 @@ input.addEventListener("input", (e) => {
   console.log(e.target.value);
 
   if (isNaN(valeur)) {
+    e.target.value = "";
+    alert("Vous venez d'appuyer sur une lettre, tout est suprimer)");
     reponse.innerHTML = "la valeur n'est pas un chiffre";
   }
   if (functionajouer == "Kilomettre") {
@@ -46,8 +53,8 @@ input.addEventListener("input", (e) => {
   if (functionajouer == "centimetre") {
     Centimetre(e.target.value);
   }
-  if (functionajouer == true) {
-    functionn(millimettre);
+  if (functionajouer == "millimettre") {
+    Milimettre(e.target.value);
   }
 });
 
@@ -90,18 +97,6 @@ function Mettre(valeur) {
   }
 }
 function Decimettre(valeur) {
-  // switch (true) {
-  //   case kilomettre:
-  //     reponse.innerHTML = valeur / 10000 + " km";
-  //   case mettre:
-  //     reponse.innerHTML = valeur + " m";
-  //   case decimettre:
-  //     reponse.innerHTML = valeur + " dm";
-  //   case centimetre:
-  //     reponse.innerHTML = valeur * 10 + " cm";
-  //   case millimettre:
-  //     reponse.innerHTML = valeur * 100 + " mm";
-  // }
   if (kilomettre == true) {
     reponse.innerHTML = valeur / 10000 + " km";
   }
@@ -119,17 +114,6 @@ function Decimettre(valeur) {
   }
 }
 function Centimetre(valeur) {
-  //   case kilomettre:
-  //     reponse.innerHTML = valeur / 100000 + " km";
-  //   case mettre:
-  //     reponse.innerHTML = valeur / 100 + " m";
-  //   case decimettre:
-  //     reponse.innerHTML = valeur / 10 + " dm";
-  //   case centimetre:
-  //     reponse.innerHTML = valeur + " cm";
-  //   case millimettre:
-  //     reponse.innerHTML = valeur * 10 + " mm";
-  // }  if (kilomettre == true) {
   if (kilomettre == true) {
     reponse.innerHTML = valeur / 100000 + " km";
   }
@@ -148,6 +132,7 @@ function Centimetre(valeur) {
   }
 }
 function Milimettre(valeur) {
+  console.log(valeur);
   if (kilomettre == true) {
     reponse.innerHTML = valeur / 1000000 + " km";
   }
@@ -168,6 +153,8 @@ function Milimettre(valeur) {
 
 ul2.forEach((li) => {
   li.addEventListener("click", (e) => {
+    document.querySelector(".active2")?.classList.remove("active2");
+    li.classList.add("active2");
     if (e.target.id == "Kilomettre2") {
       kilomettre = true;
       mettre = false;
@@ -214,7 +201,7 @@ ul2.forEach((li) => {
 });
 function execution(functionn) {
   if (kilomettre == true) {
-    Kilomettre(kilomettre);
+    functionn(kilomettre);
     kilomettre = false;
   }
   if (mettre == true) {
@@ -229,14 +216,12 @@ function execution(functionn) {
     functionn(centimetre);
     centimetre = false;
   }
-}
-if (millimettre == true) {
-  functionn(millimettre);
-  millimettre = false;
+  if (millimettre == true) {
+    functionn(millimettre);
+    millimettre = false;
+  }
 }
 
 execution(functionajouer);
 
-
-execution(functionajouer);
 
